e1634e33a168f09de3c055b75fd5667ba28b380d
Dariel <gomez.b.dariel@gmail.com>
2023-12-01 05:49:32 -0600

    Project structure

---
+++ src/app/graficas/pages/barras-dobles/barras-dobles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barras-dobles',
+  templateUrl: './barras-dobles.component.html',
+  styles: [
+  ]
+})
+export class BarrasDoblesComponent {
+
+}
