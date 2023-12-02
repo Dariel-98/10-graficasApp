3b3b961dadbaba092c88a2df82b941097e4a5795
Dariel <gomez.b.dariel@gmail.com>
2023-12-02 09:26:07 -0600

    Personalizando grafica de barras

--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -12,27 +12,30 @@ export class BarrasComponent {
 
   public barChartOptions: ChartConfiguration['options'] = {
     responsive: true,
-    // We use these empty structures as placeholders for dynamic theming.
-    scales: {
-      x: {},
-      y: {
-        min: 10,
-      },
-    },
-    plugins: {
-      legend: {
-        display: true,
-      },
-    },
   };
   public barChartType: ChartType = 'bar';
 
   public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    labels: ['2020', '2021', '2022', '2023', '2024', '2025', '2026'],
     datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series C' },
+      {
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Series A',
+        backgroundColor: '#FAEC52',
+        hoverBackgroundColor: 'blue',
+      },
+      {
+        data: [28, 48, 40, 19, 86, 27, 90],
+        label: 'Series B',
+        backgroundColor: '#9DFA52',
+        hoverBackgroundColor: 'blue',
+      },
+      {
+        data: [28, 48, 40, 19, 86, 27, 90],
+        label: 'Series C',
+        backgroundColor: '#E3FA52',
+        hoverBackgroundColor: 'blue',
+      },
     ],
   };
 
@@ -59,15 +62,17 @@ export class BarrasComponent {
 
   public randomize(): void {
     // Only Change 3 values
-    this.barChartData.datasets[0].data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      Math.round(Math.random() * 100),
-      56,
-      Math.round(Math.random() * 100),
-      40,
-    ];
+    for (let i = 0; i < this.barChartData.datasets.length; i++) {
+      this.barChartData.datasets[i].data = [
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+      ];
+    }
 
     this.chart?.update();
   }
